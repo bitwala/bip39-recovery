5ed7babfab35fd70b1cc00013013ee73a74662d8
Arturs <arturs@bitwa.la>
2018-09-18 15:04:59 +0200

    create basic form for main and backup key

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Mnemonic from "bitcore-mnemonic";
+
+const toPrivateKey = words => {
+  const code = new Mnemonic(words);
+  return code.toHDPrivateKey().xprivkey;
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      main: "",
+      backup: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    try {
+      if (this.state.main) {
+        prompt("Copy your main key", toPrivateKey(this.state.main));
+      }
+    } catch (err) {
+      alert("Error: Your main passprahse was wrong");
+      console.error(err);
+    }
+    try {
+      if (this.state.backup) {
+        prompt("Copy your backup key", toPrivateKey(this.state.backup));
+      }
+    } catch (err) {
+      alert("Error: Your backup passprahse was wrong");
+      console.error(err);
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -10,9 +50,29 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Main:
+            <input
+              required
+              type="text"
+              name="main"
+              value={this.state.main}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Backup:
+            <input
+              required
+              type="text"
+              name="backup"
+              value={this.state.backup}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
